9f8ee7570d55a2dcf44fcd98a9358154867a1812
Jeremy Kahn <jeremyckahn@gmail.com>
2018-01-12 08:38:51 -0600

    Set up app boilerplate
    Taken from https://gist.github.com/rauchg/243c5109c793bf09c052

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-export default class MyClass {
-  /**
-   * @constructs MyClass
-   */
-  constructor () {
+const app = require('express')();
+const srv = require('http').createServer(app);
+const bodyParser = require('body-parser');
 
-    /**
-     * @member MyClass#myBoolean
-     * @type {boolean}
-     * @default true
-     */
-    this.myBoolean = true;
-  }
+app.post('/', function (req, res, next) {
+  res.send('You POSTed to the micro-service!');
+});
 
-  /**
-   * @method MyClass#sayHello
-   * @returns {string} A friendly greeting.
-   */
-  sayHello () {
-    return 'Hello, World!';
-  }
-}
+srv.listen(3000, function () {
+  console.log('Listening on 3000');
+});
