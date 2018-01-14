70783d2359458e390b65d0b0b081258d672efba9
Jeremy Kahn <jeremyckahn@gmail.com>
2018-01-14 14:18:49 -0600

    Improve logging tools

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const srv = require('http').createServer(app);
 const bodyParser = require('body-parser');
 
+const { NEWS_API_KEY } = process.env;
+
+require('now-logs')(NEWS_API_KEY);
+
 app.post('/', function (req, res, next) {
   res.send('You POSTed to the micro-service!');
 });
